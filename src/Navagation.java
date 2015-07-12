9b0a38a67330d36a1a15a269b07887cb42540612
ntankard <n1457059>
2015-07-12 23:02:55 +1000

    Basic screen

---
+++ src/Navagation.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created by Nicholas on 12/07/2015.
+ */
+public class Navagation {
+    private JPanel panel1;
+    private JPanel Altitude;
+    private JPanel Left;
+    private JPanel Right;
+    private JPanel Back;
+    private JPanel Front;
+    private JPanel Top;
+    private JPanel Lateral;
+    private JPanel Navigation;
+    private JTabbedPane Status;
+}
